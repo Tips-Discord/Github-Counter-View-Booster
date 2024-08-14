3a7f7fa9e6dfeff77d0b32d1f4b8fe9e10c93a1e
Tips <150616801+Tips-Discord@users.noreply.github.com>
2024-08-14 16:10:55 +0200

    Update

--- Main.py
+++ Main.py
@@ -2,9 +2,8 @@ import requests
 import time
 import ctypes
 import threading
-from colorama import Fore, Style, init
-
-init()
+from colorama import Fore, Style, init; init()
+import re
 
 view = 0
 view_lock = threading.Lock()
@@ -12,7 +11,7 @@ proxies_lock = threading.Lock()
 proxies_list = []
 stop_event = threading.Event()
 
-def fetch_proxies_from_url():
+def fetch_proxies():
     global proxies_list
     try:
         response = requests.get("https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=ipport&format=text&timeout=5000", timeout=10)
@@ -25,7 +24,7 @@ def fetch_proxies_from_url():
     except requests.exceptions.RequestException:
         print(f"{Fore.RED}Failed to fetch proxies.{Style.RESET_ALL}")
 
-def update_console_title():
+def update_title():
     try:
         with proxies_lock:
             title = f"Tips booster | Total Views: {view}"
@@ -33,19 +32,21 @@ def update_console_title():
     except Exception:
         print(f"{Fore.RED}Failed to update console title.{Style.RESET_ALL}")
 
-def send_request(proxy, url):
+def send(proxy, url):
     global view
     while not stop_event.is_set():
         try:
+            start = time.time()
             response = requests.get(
                 url,
                 proxies={"http": f"http://{proxy}", "https": f"http://{proxy}"},
             )
+            end = time.time()
             if response.status_code == 200:
                 with view_lock:
                     view += 1
-                update_console_title()
-                print(f"{Fore.CYAN}Using proxy {proxy} | Status code {response.status_code} | Total views: {view}{Style.RESET_ALL}")
+                update_title()
+                print(f"{Fore.CYAN}Using proxy {proxy} | Status code {response.status_code} | Total views: {view} | Time: {end - start:.2f} seconds{Style.RESET_ALL}")
             else:
                 print(f"{Fore.YELLOW}Proxy {proxy} | status code {response.status_code}{Style.RESET_ALL}")
         except requests.exceptions.RequestException:
@@ -55,6 +56,14 @@ def send_request(proxy, url):
                     proxies_list.remove(proxy)
             break
 
+def get_views(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        start_views = re.search(r'<text x="102.5" y="14">([\d,]+)</text>', response.text).group(1)
+        print(f"{Fore.GREEN}Starting views: {start_views}{Style.RESET_ALL}")
+    else:
+        print(f"{Fore.RED}Failed to get views: {response.status_code}{Style.RESET_ALL}")
+
 def start_threads(url):
     threads = []
     while not stop_event.is_set():
@@ -62,7 +71,8 @@ def start_threads(url):
             if not proxies_list or stop_event.is_set():
                 break
             proxy = proxies_list.pop(0)
-        thread = threading.Thread(target=send_request, args=(proxy, url))
+
+        thread = threading.Thread(target=send, args=(proxy, url))
         thread.start()
         threads.append(thread)
 
@@ -70,11 +80,13 @@ def start_threads(url):
         thread.join()
 
 if __name__ == "__main__":
-    view_counter_url = "" # your view counter url
+    view_counter_url = input("Enter view counter url: ")
 
     try:
-        fetch_proxies_from_url()
-        update_console_title()
+        get_views(view_counter_url)
+        time.sleep(3)
+        fetch_proxies()
+        update_title()
         print(f"{Fore.GREEN}Starting {len(proxies_list)} threads.{Style.RESET_ALL}")
         start_threads(view_counter_url)
     except Exception:
